5abadb542e9d1ff0a864f091acd94c10ff5df532
Ryan Kembrey <114723629+ryankembrey@users.noreply.github.com>
2025-09-23 22:47:31 +1000

    Cherry pick code from proof-of-concept

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# FreeCAD DFM Workbench
+# FreeCAD DFM workbench
+
+> [!CAUTION]
+> NOTE THIS WORKBENCH IS IN AN UN-WORKING STATE, AND WILL BE SO UNTIL 2026
+
+FreeCAD. This workbench analyses 3D models and provides actionable feedback to
+help designers create parts that are cheaper, faster, and more reliable to
+manufacture.
+
+Currently under heavy development as part of a university capstone project.
+
+## Roadmap (subject to change)
+* [ ] Injection molding process
+    - [ ] Draft angle check
+    - [ ] Undercut check
+    - [ ] Uniform thickness check
+    - [ ] Min/Max wall thickness check
+* [ ] Add GUI
+* [ ] Support for custom standards
+
+## Contributing
+
+This is a single-student university project. Code contributions are not
+currently accepted, until the end of the project (July 2026). However, ideas,
+and bug reports are welcome! Please feel free to open an issue on the GitHub
+repository to discuss features or problems.
---
+++ checks/base_check.py
@@ -0,0 +1,55 @@
+#  ***************************************************************************
+#  *   Copyright (c) 2025 Ryan Kembrey <ryan.FreeCAD@gmail.com>              *
+#  *                                                                         *
+#  *   This file is part of the FreeCAD CAx development system.              *
+#  *                                                                         *
+#  *   This library is free software; you can redistribute it and/or         *
+#  *   modify it under the terms of the GNU Library General Public           *
+#  *   License as published by the Free Software Foundation; either          *
+#  *   version 2 of the License, or (at your option) any later version.      *
+#  *                                                                         *
+#  *   This library  is distributed in the hope that it will be useful,      *
+#  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#  *   GNU Library General Public License for more details.                  *
+#  *                                                                         *
+#  *   You should have received a copy of the GNU Library General Public     *
+#  *   License along with this library; see the file COPYING.LIB. If not,    *
+#  *   write to the Free Software Foundation, Inc., 59 Temple Place,         *
+#  *   Suite 330, Boston, MA  02111-1307, USA                                *
+#  *                                                                         *
+#  ***************************************************************************
+
+from abc import ABC, abstractmethod
+from typing import Generator, Any
+
+from OCC.Core.TopoDS import TopoDS_Face
+
+
+class BaseCheck(ABC):
+    """
+    The base class for all checks. This class defines how all checks should behave.
+    """
+
+    # A list of check types this check can perform (some can check multiple things)
+    handled_check_types = []
+
+    # A list of the Analyzer dependencies needed for this check to run.
+    # (some Checks need data from multiple Analyzers)
+    dependencies = []
+
+    # Returns a human readable string for UI uses
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    # This is the method where the check is run
+    @abstractmethod
+    def run_check(
+        self,
+        analysis_data_map,
+        parameters: dict[str, Any],
+        check_type,
+    ):
+        pass
