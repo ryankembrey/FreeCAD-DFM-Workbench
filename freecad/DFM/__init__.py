70777ca3a5a723b6b2fb37b2137cde56840b412c
Ryan Kembrey <114723629+ryankembrey@users.noreply.github.com>
2026-04-22 20:38:48 +1000

    App: Warn users of missing pythonocc installation on workbench startup

--- freecad/DFM/__init__.py
+++ freecad/DFM/__init__.py
@@ -2,3 +2,18 @@
 # SPDX-FileCopyrightText: 2025 Ryan Kembrey <ryan.FreeCAD@gmail.com>
 # SPDX-FileNotice: Part of the DFM addon.
 
+import FreeCAD as App  # type: ignore
+
+try:
+    import OCC
+except ImportError:
+    msg = (
+        "The DFM Workbench requires pythonocc-core, which is not available in this FreeCAD installation.\n"
+        "This is common when compiling FreeCAD from source without pixi.\n\n"
+        "To fix this, try one of the following:\n"
+        "  - Recompile using pixi (recommended): https://freecad.github.io/DevelopersHandbook/gettingstarted/#pixi\n"
+        "  - Use a FreeCAD AppImage, Flatpak, or package from freecad.org\n"
+        "  - On Windows/Mac, the standard installer includes pythonocc"
+    )
+    App.Console.PrintUserError(msg + "\n")
+    raise ImportError(msg)
